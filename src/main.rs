2235740d026c0a0b2c5861794de5ceb5b67f6ba9
epicadidash <dradwaitdash@gmail.com>
2023-06-18 22:59:31 +0530

    text area added

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    MainWindow::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+    export component MainWindow inherits Window {
+        Rectangle {
+        x: 10px;
+        y: 10px;
+        width: 50px;
+        height: 50px;
+        background: blue;
+    
+    TextInput {
+        text: "Replace me with a name";
+        single-line:false;
+        wrap:word-wrap;
+    }
+    }
+}
+}
